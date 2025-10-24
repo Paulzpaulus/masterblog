880125a1900406e178177dcedbc97f8d5c2357cf
Paulspaulus <pauls.schweppe@gmail.com>
2025-10-24 13:03:43 +0200

    created index file and storage_file

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    with open("storage_file.json", "r") as file:
+        blog_posts = json.load(file)
+    return render_template('index.html', posts=blog_posts)
+
+
 
 
 if __name__ == '__main__':
