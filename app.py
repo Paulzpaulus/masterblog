352906408e632109caf2b3ac0b933c9ddcac36f4
Paulspaulus <pauls.schweppe@gmail.com>
2025-10-27 13:57:48 +0100

    added POST handling/ URL /add)

--- app.py
+++ app.py
@@ -1,16 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import json
 
 app = Flask(__name__)
 
 
+def load_file(path):
+    with open(path, "r") as fileobj:
+        return json.load(fileobj)
+
+
 @app.route('/')
 def index():
-    with open("storage_file.json", "r") as file:
-        blog_posts = json.load(file)
-    return render_template('index.html', posts=blog_posts)
+    return render_template('index.html', posts=load_file("data_storage.json"))
 
+@app.route("/add", methods =["GET", "POST"])
+def add():
+    if request.method == "POST":
+        author = request.form.get("author")
+        title = request.form.get("title")
+        content = request.form.get("content")
 
+        blogentries = load_file("data_storage.json")
+        if len(blogentries) == 0:
+            next_id = 1
+        else:
+            next_id = blogentries[-1]["id"] + 1
+
+        new_data = {
+            "id": next_id,
+            "author":author,
+            "title": title,
+            "content":content
+        }
+        blogentries.append(new_data)
+
+        with open("data_storage.json", "w") as fileobj:
+            json.dump(blogentries, fileobj, indent= 2)
+        return redirect(url_for('index'))
+    return render_template("add.html")
 
 
 if __name__ == '__main__':
